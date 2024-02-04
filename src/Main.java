d755ee6c36fcb3bc3f89c52ad8d58da9cb0b0fec
Dmitry <dgorodilov@mail.ru>
2024-02-04 19:08:19 +0300

    changed 04022024

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
@@ -12,17 +11,17 @@ public class Main {
         GameProgress game1 = new GameProgress(15, 100, 3, 115.7);
         GameProgress game2 = new GameProgress(45, 500, 5, 320.71);
         GameProgress game3 = new GameProgress(100, 900, 11, 950.77);
-        saveGame("C:\\Users\\User\\OneDrive\\Рабочий стол\\Работа\\Тест\\Учеба\\Games\\savegames\\game1.dat", game1);
-        saveGame("C:\\Users\\User\\OneDrive\\Рабочий стол\\Работа\\Тест\\Учеба\\Games\\savegames\\game2.dat", game2);
-        saveGame("C:\\Users\\User\\OneDrive\\Рабочий стол\\Работа\\Тест\\Учеба\\Games\\savegames\\game3.dat", game3);
+        saveGame("C:\\Java\\Учеба\\Games\\savegames\\game1.dat", game1);
+        saveGame("C:\\Java\\Учеба\\Games\\savegames\\game2.dat", game2);
+        saveGame("C:\\Java\\Учеба\\Games\\savegames\\game3.dat", game3);
         ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("C:\\Users\\User\\OneDrive\\Рабочий стол\\Работа\\Тест\\Учеба\\Games\\savegames\\game1.dat");
-        arrayList.add("C:\\Users\\User\\OneDrive\\Рабочий стол\\Работа\\Тест\\Учеба\\Games\\savegames\\game2.dat");
-        arrayList.add("C:\\Users\\User\\OneDrive\\Рабочий стол\\Работа\\Тест\\Учеба\\Games\\savegames\\game3.dat");
-        zipFiles("C:\\Users\\User\\OneDrive\\Рабочий стол\\Работа\\Тест\\Учеба\\Games\\savegames\\zip.zip", arrayList);
-        File game1Dat = new File("C:\\Users\\User\\OneDrive\\Рабочий стол\\Работа\\Тест\\Учеба\\Games\\savegames\\game1.dat");
-        File game2Dat = new File("C:\\Users\\User\\OneDrive\\Рабочий стол\\Работа\\Тест\\Учеба\\Games\\savegames\\game2.dat");
-        File game3Dat = new File("C:\\Users\\User\\OneDrive\\Рабочий стол\\Работа\\Тест\\Учеба\\Games\\savegames\\game3.dat");
+        arrayList.add("C:\\Java\\Учеба\\Games\\savegames\\game1.dat");
+        arrayList.add("C:\\Java\\Учеба\\Games\\savegames\\game2.dat");
+        arrayList.add("C:\\Java\\Учеба\\Games\\savegames\\game3.dat");
+        zipFiles("C:\\Java\\Учеба\\Games\\savegames\\zip.zip", arrayList);
+        File game1Dat = new File("C:\\Java\\Учеба\\Games\\savegames\\game1.dat");
+        File game2Dat = new File("C:\\Java\\Учеба\\Games\\savegames\\game2.dat");
+        File game3Dat = new File("C:\\Java\\Учеба\\Games\\savegames\\game3.dat");
         if (game1Dat.delete()) System.out.println("Файл \"game1.dat\" удален");
         if (game2Dat.delete()) System.out.println("Файл \"game2.dat\" удален");
         if (game3Dat.delete()) System.out.println("Файл \"game3.dat\" удален");
@@ -38,21 +37,25 @@ public class Main {
     }
 
     private static void zipFiles(String path, List<String> arrayList) {
-        try (ZipOutputStream zouts = new ZipOutputStream(new FileOutputStream(path))) {
-            for (String arr : arrayList) {
-                try (FileInputStream fis = new FileInputStream(arr)) {
-                    ZipEntry entry = new ZipEntry(arr);
-                    zouts.putNextEntry(entry);
-                    while (fis.available() > 0) {
-                        zouts.write(fis.read());
-                    }
-                    zouts.closeEntry();
-                } catch (Exception ex) {
-                    System.out.println(ex.getMessage());
-                }
+        try {
+
+            FileOutputStream fos = new FileOutputStream(path);
+            ZipOutputStream zos = new ZipOutputStream(fos);
+
+            for (String aFile : arrayList) {
+                zos.putNextEntry(new ZipEntry(new File(aFile).getName()));
+
+                byte[] bytes = Files.readAllBytes(Paths.get(aFile));
+                zos.write(bytes, 0, bytes.length);
+                zos.closeEntry();
             }
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+
+            zos.close();
+
+        } catch (FileNotFoundException ex) {
+            System.err.println("A file does not exist: " + ex);
+        } catch (IOException ex) {
+            System.err.println("I/O error: " + ex);
         }
     }
 }
